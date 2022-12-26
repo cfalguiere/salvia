4320690408d4d20a87971a6bdd7954360f737720
Claude Falguiere <cfalguiere+git@gmail.com>
2022-12-26 15:38:44 +0000

    enhanced build file

--- examples/labbench/iris/noxfile.py
+++ examples/labbench/iris/noxfile.py
@@ -14,23 +14,32 @@ nox.options.sessions = ['nblint']
 @nox.session(venv_backend=VENV_BACKEND, python=VENV_PYTHON_VERSION)
 def nblint(session):
     session.install('nblint')
-    session.install('sagemaker==2.120.0')
+    session.install('sagemaker==2.120.0')  # TODO env setup or requirements
 
     import os
+    error = 0
     for root, dirs, files in os.walk('./notebooks/'):
         if 'output' not in root:
             for name in files:
                 if name.endswith('.ipynb'):
                     if 'checkpoint' not in name:
                         filename = os.path.join(root, name)
-                        session.run(
-                                'nblint',
-                                '--linter',
-                                'pyflakes',
-                                filename)
-    # TODO ignore magic
+                        try:
+                            session.run(
+                                    'nblint',
+                                    '--linter',
+                                    'pyflakes',
+                                    filename)
+                            # FIXME nblint does not seem to report errors
+                        except:
+                            session.log(f"lint error in {filename}")
+                            error += 1
+
+    if error > 0:
+        session.error(f"{error} were found in nblint") 
 
 
+# FIXME does not work fine with sagemaker
 @nox.session(venv_backend=VENV_BACKEND, python=VENV_PYTHON_VERSION)
 def nbcheck(session):
     session.install('nbconvert')
