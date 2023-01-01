27c4c26721346fa6f65ecedc628a8edb6fefb0a2
Claude Falguiere <cfalguiere+git@gmail.com>
2023-01-01 22:10:58 +0000

    added missing files

---
+++ examples/factory/abalone/abalone/__version__.py
@@ -0,0 +1,9 @@
+"""Metadata for the pipelines package."""
+
+__title__ = "abalone"
+__description__ = "abalone - template package"
+__version__ = "0.0.1"
+__author__ = "<Your Name>"
+__author_email__ = "<your-email@python.org>"
+__license__ = "Apache 2.0"
+__url__ = "<https://your-website>"
